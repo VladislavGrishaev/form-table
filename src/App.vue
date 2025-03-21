9f7e611ab2513a89de574ef1df57ef994ad0d627
VladislavG <slava.violet94@yandex.ru>
2025-03-21 19:50:06 +0500

    анимация смены компонентов контента табов

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,31 @@
 <script setup>
-
 import NavTabs from "./components/NavTabs.vue";
 import '../src/assets/style.css'
-import Form from "./components/Form.vue";
-import Table from "./components/Table.vue";
+
 
 </script>
 
 <template>
   <NavTabs/>
-<!--  <router-view/>-->
-  <Form/>
-  <Table/>
+  <transition name="fade-slide" mode="out-in">
+    <router-view />
+  </transition>
 </template>
 
+<style scoped>
+.fade-slide-enter-active, .fade-slide-leave-active {
+  transition: opacity 0.5s ease, transform 0.5s ease;
+}
 
+.fade-slide-enter-from {
+  opacity: 0;
+  transform: translateY(20px);
+}
+
+.fade-slide-leave-to {
+  opacity: 0;
+  transform: translateY(-20px);
+}
+</style>
 
 
